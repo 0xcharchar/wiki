dbfa43bc2788cb20d32a8985116744a170b6dfda
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-08-11 21:03:03 +0100

    Creating directory, coping style file

--- .github/actions/vimwiki-html-docker-action/entrypoint.sh
+++ .github/actions/vimwiki-html-docker-action/entrypoint.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
-vim --not-a-term +VimwikiAll2HTML +quit $HOME/vimwiki/index.wiki
+mkdir -p /github/workspace/public_html
+cp /github/workspace/templates/style.css /github/workspace/public_html/
+
+vim --not-a-term +VimwikiAll2HTML +quit /github/workspace/index.wiki
