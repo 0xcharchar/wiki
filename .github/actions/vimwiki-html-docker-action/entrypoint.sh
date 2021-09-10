15a892e7171290b316e04b86f61cd639889d2739
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-09-10 21:54:53 +0100

    move images, copy images

--- .github/actions/vimwiki-html-docker-action/entrypoint.sh
+++ .github/actions/vimwiki-html-docker-action/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-mkdir -p /github/workspace/public_html
+mkdir -p /github/workspace/public_html/images
 cp /github/workspace/templates/style.css /github/workspace/public_html/
+cp -R /github/workspace/images/* /github/workspace/public_html/images/
 
 vim --not-a-term +VimwikiAll2HTML +quit /github/workspace/index.wiki
