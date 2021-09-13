cd4fdfc35a7b65cf9373fa35dc88255a9f5665fe
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-09-13 22:34:00 +0100

    less safe rendering but can't figure out why ash is failing

--- .github/actions/vimwiki-html-docker-action/entrypoint.sh
+++ .github/actions/vimwiki-html-docker-action/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-cd /github/workspace
+cd $GITHUB_WORKSPACE
 
 # create output folder
 mkdir -p public_html
 
 # copy directory structure, wiki files, and images
-tar cf - --exclude=public_html --exclude='.[^/]*' --exclude="*.sh" . | (cd public_html && tar xvf - )
+tar cf - --exclude=public_html --exclude='.[^/]*' --exclude="*.sh" . | (cd public_html && tar xf - )
 
 # remove wiki files
 find public_html -type f -name "*.wiki" -delete
@@ -14,14 +14,17 @@ find public_html -type f -name "*.wiki" -delete
 # copy over style
 cp .meta/style.css public_html/style.css
 
-find . -name '*.wiki' -print0 -not -path -"./public_html" | while read -d $'\0' -r file ; do
-  printf 'Found: '"'%s'"'\n' "${file##*/}"
+# convert all wiki files
+# lookin in this '.' directory so that the output is easier and don't need to do string substition
+find . -name '*.wiki' | while read file ; do
   outname=$(echo "${file}" | sed -e "s/\.wiki/\.html/g")
-  #echo $outname
+  #printf 'DEBUG - Found '"'%s'"', converted to '"'%s'"'\n' "${file##*/}" "${outname}"
+
   pandoc -f vimwiki -t html5 \
     --lua-filter=.meta/fixlinks.lua \
     --template=.meta/templates/default.html \
     --highlight-style=pygments \
     --css=style.css \
-    "${file}" -o "public_html/$outname"
+    --verbose \
+    "${file}" -o "public_html/${outname}"
 done
