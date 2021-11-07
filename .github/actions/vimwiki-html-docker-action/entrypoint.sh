f464ca4f766917c73d2a5a5a636ed6c3113c5a10
0xCharChar <87212793+0xcharchar@users.noreply.github.com>
2021-11-07 15:31:39 +0000

    cleanup, hopefully better source code rendering

--- .github/actions/vimwiki-html-docker-action/entrypoint.sh
+++ .github/actions/vimwiki-html-docker-action/entrypoint.sh
@@ -8,23 +8,38 @@ mkdir -p public_html
 # copy directory structure, wiki files, and images
 tar cf - --exclude=public_html --exclude='.[^/]*' --exclude="*.sh" . | (cd public_html && tar xf - )
 
-# remove wiki files
+# remove wiki files to keep structure
 find public_html -type f -name "*.wiki" -delete
 
 # copy over style
 cp .meta/style.css public_html/style.css
 
 # convert all wiki files
-# lookin in this '.' directory so that the output is easier and don't need to do string substition
+#
+# look in this '.' directory (GITHUB_WORKSPACE) so that the output is easier
+# to work with
 find . -name '*.wiki' | while read file ; do
   outname=$(echo "${file}" | sed -e "s/\.wiki/\.html/g")
-  #printf 'DEBUG - Found '"'%s'"', converted to '"'%s'"'\n' "${file##*/}" "${outname}"
 
   pandoc -f vimwiki -t html5 \
     --lua-filter=.meta/fixlinks.lua \
     --template=.meta/templates/default.html \
+    --syntax-definition=.meta/solidity.xml \
     --highlight-style=pygments \
     --css=style.css \
     --verbose \
     "${file}" -o "public_html/${outname}"
+
+  # Using sed to remove whitespace at the beginning of each line
+  # This is a cheap hack to work around problems with pandoc putting
+  # additional whitespace in `pre` tags causing source code to be
+  # formatted incorrectly. This works because rendered source
+  # code wraps code formatting whitespace in `<span>` so we can safely
+  # strip whitespace in front of the spans. AFAIK, this can't
+  # be done in a filter due to this not being an AST problem.
+  #
+  # There are some caveats where no `<span>` tags are rendered:
+  #   1. if `--no-highlight` is used
+  #   2. if the source code is unknown (shoutout to solidity)
+  sed "s/^[ \t]*//g" -i "public_html/${outname}"
 done
